14c21dd098f978ed225351bc51b2ede796ef6940
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-05-01 15:07:11 +0300

    Final Project refactoring

--- src/test/java/one_a_site/models/UserStatic.java
+++ src/test/java/one_a_site/models/UserStatic.java
@@ -6,4 +6,11 @@ import lombok.Getter;
 public class UserStatic {
     String loginName = "mihejevs.igors@gmail.com";
     String password = "IM020660";
+    String firstName = "Igors";
+    String secondName = "Mihejevs";
+    String mobile = "29111111";
+    String city = "Riga";
+    String address = "Brivibas str.100-1";
+    String postIndex = "LV-1111";
+    String additionalInformation = "Door code - 1234";
 }
