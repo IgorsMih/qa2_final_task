3d2506761f6fa68f12bdecd0cd97f519b07032ce
Igors Mihejevs <mihejevs.igors@gmail.com>
2022-05-03 13:45:47 +0300

    Final Project refactoring

--- src/test/java/one_a_site/pages/ProductPagePom.java
+++ src/test/java/one_a_site/pages/ProductPagePom.java
@@ -13,9 +13,9 @@ public class ProductPagePom extends BasePagePom{
     private final By openDatortehnikaSubmenu = By.xpath("//a[@class='submenu-lvl1__link']");
     private final By openPortativieDatori = By.xpath("//img[@src='https://sb.ksd-images.lt/onea_lv/images/rich_texts/Articles/limonade5/2/3_PORTATIVIE_DATORI_2.jpg']");
     private final By openPortativieDatoriBiznesam = By.xpath("//div[@class='catalog-taxons-visual-filter-glide-slider__slide glide__slide']");
-    private final By openAsusX515 = By.xpath("//div[@class='catalog-taxons-product catalog-taxons-product--grid-view'][1]");
-    private final By getNameAsusX515 = By.xpath("//div[@class='product-righter google-rich-snippet']/h1");
-    private final By getPriceAsusX515 = By.xpath("//span[@class='price'][1]");
+    private final By openProduct = By.xpath("//div[@class='catalog-taxons-product catalog-taxons-product--grid-view'][1]");
+    private final By getProductName = By.xpath("//div[@class='product-righter google-rich-snippet']/h1");
+    private final By getProductPrice = By.xpath("//span[@class='price'][1]");
     private final By addProductToShoppingCart = By.id("add_to_cart_btn");
     private final By goToShoppingCart = By.xpath("//a[@class='main-button']");
 
@@ -29,12 +29,12 @@ public class ProductPagePom extends BasePagePom{
         driver.findElement(openPortativieDatori).click();
         action.moveToElement(driver.findElement(openPortativieDatoriBiznesam)).click().perform();
         sleepMode(3000);
-        action.moveToElement(driver.findElement(openAsusX515)).click().perform();
+        action.moveToElement(driver.findElement(openProduct)).click().perform();
     }
 
     public void getNamePriceAndLink() {
-        product.setProductName(driver.findElement(getNameAsusX515).getText());
-        product.setProductPrice(driver.findElement(getPriceAsusX515).getText());
+        product.setProductName(driver.findElement(getProductName).getText());
+        product.setProductPrice(driver.findElement(getProductPrice).getText());
         product.setProductLink(driver.getCurrentUrl());
         System.out.println("Name  = " + product.getProductName() + "\nPrice = " + product.getProductPrice() + "\nLink  = " + product.getProductLink());
     }
